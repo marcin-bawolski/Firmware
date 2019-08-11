33fdc6ca34d2b2c18de4ab0550dfa4551d293931
Marcin <marcin@OptiPlex-780.bawolski.net>
2019-08-11 11:09:12 +0200

    Testing user command

--- boards/nxp/fmuk66-v3/default.cmake
+++ boards/nxp/fmuk66-v3/default.cmake
@@ -110,6 +110,7 @@ px4_add_board(
 		#matlab_csv_serial
 		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
 		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		px4_uwb_app
 		rover_steering_control # Rover example app
 		segway
 		uuv_example_app
